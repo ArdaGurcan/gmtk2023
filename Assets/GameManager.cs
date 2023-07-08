76bd570eb2120731a28ccb8bcce1ffc438ed785b
ArdaGurcan <ardagurcan2004@gmail.com>
2023-07-09 00:44:29 +0300

    get voicelines

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -10,10 +10,12 @@ public class GameManager : MonoBehaviour
     StanleyController stanleyController;
     public List<string> voicelines;
     public GameObject buttonPrefab;
-
+    [SerializeField]
+    Dictionary<string,List<GameObject>> buttonsDict;
 
     void Start()
     {
+        buttonsDict = new Dictionary<string, List<GameObject>>();
         stanley = GameObject.FindGameObjectWithTag("Stanley");
         stanleyController = stanley.GetComponent<StanleyController>();
         stanley.SetActive(false);
@@ -23,21 +25,29 @@ public class GameManager : MonoBehaviour
         {
             // instantiate button for each voiceline under canvas
             Transform button = Instantiate(buttonPrefab, canvas.transform.position, canvas.rotation, canvas).transform;
-            
+
             button.GetComponent<RectTransform>().anchoredPosition = new Vector3(-50, ypos, 0);
 
             Button uiButton = button.GetComponent<Button>();
             uiButton.onClick.AddListener(delegate{stanleyController.Step(voiceline);});
             
             button.GetChild(0).GetComponent<TMPro.TextMeshProUGUI>().text = voiceline.Split('|')[0];
+            List<GameObject> buttons = null;
+            if (!buttonsDict.TryGetValue(voiceline.Split('|')[1], out buttons)) {
+                buttons = new List<GameObject>();
+            }
+            buttons.Add(button.gameObject);
+
+            buttonsDict[voiceline.Split('|')[1]] = buttons;
             ypos -= 40;
+
         }
     }
 
     public void SpawnStanley() {
         stanley.SetActive(true);
         List<string> options = stanleyController.GetOptions();
-
+        
     }
 
 
