0203cd671ef0bd2bcba9b9f00c74a6d744332be7
ArdaGurcan <ardagurcan2004@gmail.com>
2023-07-08 23:13:11 +0300

    start game manager

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,17 +7,26 @@ public class GameManager : MonoBehaviour
     bool inEditMode = true;
     GameObject stanley;
     StanleyController stanleyController;
-    
+    public List<string> voicelines;
+    public GameObject buttonPrefab;
+
 
     void Start()
     {
         stanley = GameObject.FindGameObjectWithTag("Stanley");
         stanleyController = stanley.GetComponent<StanleyController>();
         stanley.SetActive(false);
+
+        foreach (string voiceline in voicelines)
+        {
+            // instantiate button for each voiceline under canvas
+        }
     }
 
     public void SpawnStanley() {
         stanley.SetActive(true);
+        List<string> options = stanleyController.GetOptions();
+
     }
 
 
