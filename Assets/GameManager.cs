49c8e7ccb3d1459e5b3a3c649e7c7df6e3db8276
ArdaGurcan <ardagurcan2004@gmail.com>
2023-07-08 23:55:19 +0300

    generate voiceline buttons programatically

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -16,10 +17,20 @@ public class GameManager : MonoBehaviour
         stanley = GameObject.FindGameObjectWithTag("Stanley");
         stanleyController = stanley.GetComponent<StanleyController>();
         stanley.SetActive(false);
-
+        Transform canvas = GameObject.Find("Canvas").transform;
+        float ypos = 0;
         foreach (string voiceline in voicelines)
         {
             // instantiate button for each voiceline under canvas
+            Transform button = Instantiate(buttonPrefab, canvas.transform.position, canvas.rotation, canvas).transform;
+            
+            button.GetComponent<RectTransform>().anchoredPosition = new Vector3(-50, ypos, 0);
+
+            Button uiButton = button.GetComponent<Button>();
+            uiButton.onClick.AddListener(delegate{stanleyController.Step(voiceline);});
+            
+            button.GetChild(0).GetComponent<TMPro.TextMeshProUGUI>().text = voiceline.Split('|')[0];
+            ypos -= 40;
         }
     }
 
